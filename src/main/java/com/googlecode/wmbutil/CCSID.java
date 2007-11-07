ba3b438914b91bbaa407801c26b9d0dcbba539cb
protocol7 <protocol7@68ca202e-fa3b-0410-97e9-59b6ee23507d>
2007-11-07 09:57:21 +0000

    Adding class with constants for some common CCSIDs
    git-svn-id: http://wmb-util.googlecode.com/svn/wmb-util/trunk@51 68ca202e-fa3b-0410-97e9-59b6ee23507d

---
+++ src/main/java/com/googlecode/wmbutil/CCSID.java
@@ -0,0 +1,12 @@
+package com.googlecode.wmbutil;
+
+public class CCSID {
+
+	public static final int UTF16_BIG_ENDIAN = 1200;
+	public static final int UTF16_LITTLE_ENDIAN = 1202;
+	public static final int UTF8 = 1208;
+	public static final int ISO_88591 = 819;
+	public static final int ISO_LATIN1 = ISO_88591;
+	public static final int ASCII = 437;
+	public static final int EBCDIC_SWE_FIN = 278;
+}
