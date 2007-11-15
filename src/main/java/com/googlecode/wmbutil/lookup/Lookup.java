99e9d9be2f4777433c45e9d55bb645067d1d91d5
protocol7 <protocol7@68ca202e-fa3b-0410-97e9-59b6ee23507d>
2007-11-15 08:37:25 +0000

    Added items remotely
    C:\home\niklas\workspaces\calle\wmb-util\src\main\java\com\googlecode\wmbutil\lookup\CacheRefreshException.java
    C:\home\niklas\workspaces\calle\wmb-util\src\main\java\com\googlecode\wmbutil\lookup\JdbcLookupDataSource.java
    C:\home\niklas\workspaces\calle\wmb-util\src\main\java\com\googlecode\wmbutil\lookup\Lookup.java
    C:\home\niklas\workspaces\calle\wmb-util\src\main\java\com\googlecode\wmbutil\lookup\LookupCacheException.java
    C:\home\niklas\workspaces\calle\wmb-util\src\main\java\com\googlecode\wmbutil\lookup\LookupData.java
    C:\home\niklas\workspaces\calle\wmb-util\src\main\java\com\googlecode\wmbutil\lookup\LookupDataSource.java
    C:\home\niklas\workspaces\calle\wmb-util\src\main\java\com\googlecode\wmbutil\lookup\StaleCacheException.java
    C:\home\niklas\workspaces\calle\wmb-util\src\main\java\com\googlecode\wmbutil\lookup\Cache.java
    
    
    git-svn-id: http://wmb-util.googlecode.com/svn/wmb-util/trunk@67 68ca202e-fa3b-0410-97e9-59b6ee23507d

---
+++ src/main/java/com/googlecode/wmbutil/lookup/Lookup.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2007 (C) Callista Enterprise.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ *
+ *	http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
+
+package com.googlecode.wmbutil.lookup;
+
+/**
+ * Provides a simple way of looking up key-value values
+ * from typically a database. Values are also cached to 
+ * increase performance and reliability.
+ */
+public class Lookup {
+
+	private LookupDataSource ds;
+	private Cache cache;
+	
+	/**
+	 * Create a lookup instance
+	 * @param ds The data source to use for loading data
+	 * @param componentName The name of the component for which
+	 * 		the lookup will be performed
+	 * @throws LookupCacheException If the cache can not be loaded or refreshed.
+	 */
+	public Lookup(LookupDataSource ds, String componentName) throws LookupCacheException {
+		this.cache = Cache.getCache(componentName);
+		this.ds = ds;
+	}
+
+	/**
+	 * Create a lookup instance using the default JDBC data source
+	 * @param componentName The name of the component for which
+	 * 		the lookup will be performed
+	 * @throws LookupCacheException If the cache can not be loaded or refreshed.
+	 */
+	public Lookup(String componentName) throws LookupCacheException {
+		this.cache = Cache.getCache(componentName);
+		this.ds = new JdbcLookupDataSource();
+	}
+
+	/**
+	 * Looks up the value for the provided key.
+	 * @param key The key to look up
+	 * @return The value for the key, or null if the key is not found
+	 * @throws LookupCacheException If the cache can not be loaded or refreshed.
+	 */
+	public String lookupValue(String key) throws LookupCacheException {
+		return cache.lookupValue(key, ds);
+	}
+}
