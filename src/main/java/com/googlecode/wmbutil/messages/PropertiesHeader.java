58fbc90e079a8c7e480695c3a8e121c2435a61f0
johannescarlen <johannescarlen@68ca202e-fa3b-0410-97e9-59b6ee23507d>
2008-04-09 08:26:58 +0000

    Added get/set for topic
    git-svn-id: http://wmb-util.googlecode.com/svn/wmb-util/trunk@108 68ca202e-fa3b-0410-97e9-59b6ee23507d

--- src/main/java/com/googlecode/wmbutil/messages/PropertiesHeader.java
+++ src/main/java/com/googlecode/wmbutil/messages/PropertiesHeader.java
@@ -23,88 +23,99 @@ import com.ibm.broker.plugin.MbMessage;
 
 public class PropertiesHeader extends Header {
 
-	public static final String PARSER_MQ_PROPERTIES = "MQPROPERTYPARSER";
-	public static final String PARSER_WS_PROPERTIES = "WSPROPERTYPARSER";
-	
-	private static final String ELEMENT_NAME = "Properties";
+    public static final String PARSER_MQ_PROPERTIES = "MQPROPERTYPARSER";
+    public static final String PARSER_WS_PROPERTIES = "WSPROPERTYPARSER";
 
-	private static final String PROPERTY_MSG_SET = "MessageSet";
-	private static final String PROPERTY_MSG_TYPE = "MessageType";
-	private static final String PROPERTY_FORMAT = "MessageFormat";
+    private static final String ELEMENT_NAME = "Properties";
 
-   	
-	public static PropertiesHeader wrap(MbMessage msg, boolean readOnly) throws MbException {
-		MbElement elm = msg.getRootElement().getFirstElementByPath("/" + ELEMENT_NAME);
+    private static final String PROPERTY_MSG_SET = "MessageSet";
+    private static final String PROPERTY_MSG_TYPE = "MessageType";
+    private static final String PROPERTY_FORMAT = "MessageFormat";
+    private static final String PROPERTY_TOPIC = "Topic";
 
-		if(elm == null) {
-			throw new NiceMbException("Failed to find " + ELEMENT_NAME);
-		}
-		
-		return new PropertiesHeader(elm, readOnly);
-	}
+    public static PropertiesHeader wrap(MbMessage msg, boolean readOnly) throws MbException {
+        MbElement elm = msg.getRootElement().getFirstElementByPath("/" + ELEMENT_NAME);
 
-	public static PropertiesHeader create(MbMessage msg) throws MbException {
-		return create(msg, PARSER_MQ_PROPERTIES);
-	}
-	
-	public static PropertiesHeader create(MbMessage msg, String parser) throws MbException {
-		if(has(msg)) {
-			throw new NiceMbException("Already have " + ELEMENT_NAME + " header");
-		}
+        if (elm == null) {
+            throw new NiceMbException("Failed to find " + ELEMENT_NAME);
+        }
 
-		MbElement elm = msg.getRootElement().createElementAsLastChild(parser);
-		
-		PropertiesHeader properties = new PropertiesHeader(elm, false); 
-		
-		
-		return properties;
-	}
-	
-	public static PropertiesHeader wrapOrCreate(MbMessage msg) throws MbException {
-		if(has(msg)) {
-			return wrap(msg, false);
-		} else {
-			return create(msg);
-		}
-	}
+        return new PropertiesHeader(elm, readOnly);
+    }
 
-	public static PropertiesHeader remove(MbMessage msg) throws MbException {
-		MbElement elm = msg.getRootElement().getFirstElementByPath("/" + ELEMENT_NAME);
-		
-		if(elm != null) {
-			elm.detach();
-			return new PropertiesHeader(elm, true);
-		} else {
-			throw new NiceMbException("Failed to find " + ELEMENT_NAME + " header");
-		}		
-	}
+    public static PropertiesHeader create(MbMessage msg) throws MbException {
+        return create(msg, PARSER_MQ_PROPERTIES);
+    }
 
-	public static boolean has(MbMessage msg) throws MbException {
-		MbElement elm = msg.getRootElement().getFirstElementByPath("/" + ELEMENT_NAME);
-		return elm != null;
-	}
-	
-	private PropertiesHeader(MbElement elm, boolean readOnly) throws MbException {
-		super(elm, readOnly);
-	}
+    public static PropertiesHeader create(MbMessage msg, String parser) throws MbException {
+        if (has(msg)) {
+            throw new NiceMbException("Already have " + ELEMENT_NAME + " header");
+        }
 
-	public String getMessageFormat() throws MbException {
-		return getStringValue(PROPERTY_FORMAT);
-	}
-	public void setMessageFormat(String messageFormat) throws MbException {
-		setStringValue(PROPERTY_FORMAT, messageFormat);
-	}
-	public String getMessageSet() throws MbException {
-		return getStringValue(PROPERTY_MSG_SET);
-	}
-	public void setMessageSet(String messageSet) throws MbException {
-		setStringValue(PROPERTY_MSG_SET, messageSet);
-	}
-	public String getMessageType() throws MbException {
-		return getStringValue(PROPERTY_MSG_TYPE);
-	}
-	public void setMessageType(String messageType) throws MbException {
-		setStringValue(PROPERTY_MSG_TYPE, messageType);
-	}
+        MbElement elm = msg.getRootElement().createElementAsLastChild(parser);
+
+        PropertiesHeader properties = new PropertiesHeader(elm, false);
+
+        return properties;
+    }
+
+    public static PropertiesHeader wrapOrCreate(MbMessage msg) throws MbException {
+        if (has(msg)) {
+            return wrap(msg, false);
+        } else {
+            return create(msg);
+        }
+    }
+
+    public static PropertiesHeader remove(MbMessage msg) throws MbException {
+        MbElement elm = msg.getRootElement().getFirstElementByPath("/" + ELEMENT_NAME);
+
+        if (elm != null) {
+            elm.detach();
+            return new PropertiesHeader(elm, true);
+        } else {
+            throw new NiceMbException("Failed to find " + ELEMENT_NAME + " header");
+        }
+    }
+
+    public static boolean has(MbMessage msg) throws MbException {
+        MbElement elm = msg.getRootElement().getFirstElementByPath("/" + ELEMENT_NAME);
+        return elm != null;
+    }
+
+    private PropertiesHeader(MbElement elm, boolean readOnly) throws MbException {
+        super(elm, readOnly);
+    }
+
+    public String getMessageFormat() throws MbException {
+        return getStringValue(PROPERTY_FORMAT);
+    }
+
+    public void setMessageFormat(String messageFormat) throws MbException {
+        setStringValue(PROPERTY_FORMAT, messageFormat);
+    }
+
+    public String getMessageSet() throws MbException {
+        return getStringValue(PROPERTY_MSG_SET);
+    }
+
+    public void setMessageSet(String messageSet) throws MbException {
+        setStringValue(PROPERTY_MSG_SET, messageSet);
+    }
+
+    public String getMessageType() throws MbException {
+        return getStringValue(PROPERTY_MSG_TYPE);
+    }
+
+    public void setMessageType(String messageType) throws MbException {
+        setStringValue(PROPERTY_MSG_TYPE, messageType);
+    }
+
+    public String getTopic() throws MbException {
+        return getStringValue(PROPERTY_TOPIC);
+    }
+
+    public void setTopic(String topic) throws MbException {
+        setStringValue(PROPERTY_TOPIC, topic);
+    }
 }
-
