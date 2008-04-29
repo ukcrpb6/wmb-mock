ff186bd6f267079efce7170ee726d0d29bb9f864
johannescarlen <johannescarlen@68ca202e-fa3b-0410-97e9-59b6ee23507d>
2008-04-29 05:27:30 +0000

    Numeric getField-methods checks first if Integer type
    git-svn-id: http://wmb-util.googlecode.com/svn/wmb-util/trunk@118 68ca202e-fa3b-0410-97e9-59b6ee23507d

--- src/main/java/com/googlecode/wmbutil/messages/TdsRecord.java
+++ src/main/java/com/googlecode/wmbutil/messages/TdsRecord.java
@@ -32,227 +32,250 @@ import com.ibm.broker.plugin.MbTimestamp;
 
 public class TdsRecord extends MbElementWrapper {
 
-    public TdsRecord(MbElement wrappedElm, boolean readOnly) throws MbException {
-        super(wrappedElm, readOnly);
-    }
+	public TdsRecord(MbElement wrappedElm, boolean readOnly) throws MbException {
+		super(wrappedElm, readOnly);
+	}
 
-    public Object getField(String name) throws MbException {
-        MbElement elm = getMbElement().getFirstElementByPath(name);
+	public Object getField(String name) throws MbException {
+		MbElement elm = getMbElement().getFirstElementByPath(name);
 
-        if (elm != null) {
-            return elm.getValue();
-        } else {
-            throw new NiceMbException("Unknown field: " + name);
-        }
-    }
+		if (elm != null) {
+			return elm.getValue();
+		} else {
+			throw new NiceMbException("Unknown field: " + name);
+		}
+	}
 
-    public void setField(String name, Object value) throws MbException {
-        checkReadOnly();
+	public void setField(String name, Object value) throws MbException {
+		checkReadOnly();
 
-        MbElement elm = getMbElement().getFirstElementByPath(name);
-        if (elm == null) {
-            elm = getMbElement().createElementAsLastChild(MbElement.TYPE_NAME_VALUE, name, value);
-        } else {
-            elm.setValue(value);
-        }
-    }
+		MbElement elm = getMbElement().getFirstElementByPath(name);
+		if (elm == null) {
+			elm = getMbElement().createElementAsLastChild(
+					MbElement.TYPE_NAME_VALUE, name, value);
+		} else {
+			elm.setValue(value);
+		}
+	}
 
-    public String getStringField(String name) throws MbException {
-    	Object o = getField(name);
-    	
-    	if(o != null) {
-    		return o.toString();
-    	} else {
-    		return null;
-    	}
-    }
+	public String getStringField(String name) throws MbException {
+		Object o = getField(name);
 
-    public int getIntField(String name) throws MbException {
-        return ((Integer) getField(name)).intValue();
-    }
+		if (o != null) {
+			return o.toString();
+		} else {
+			return null;
+		}
+	}
 
-    public long getLongField(String name) throws MbException {
-        return ((Long) getField(name)).longValue();
-    }
+	public int getIntField(String name) throws MbException {
+		return ((Integer) getField(name)).intValue();
+	}
 
-    public float getFloatField(String name) throws MbException {
-        return ((Float) getField(name)).floatValue();
-    }
+	public long getLongField(String name) throws MbException {
+		if (getField(name) instanceof Integer)
+			return ((Integer) getField(name)).longValue();
+		else
+			return ((Long) getField(name)).longValue();
+	}
 
-    public double getDoubleValue(String name) throws MbException {
-        return ((Double) getField(name)).doubleValue();
-    }
+	public float getFloatField(String name) throws MbException {
+		if (getField(name) instanceof Integer)
+			return ((Integer) getField(name)).floatValue();
+		else
+			return ((Float) getField(name)).floatValue();
+	}
 
-    public boolean getBooleanValue(String name) throws MbException {
-        return ((Boolean) getField(name)).booleanValue();
-    }
+	public double getDoubleValue(String name) throws MbException {
+		if (getField(name) instanceof Integer)
+			return ((Integer) getField(name)).doubleValue();
+		else
+			return ((Double) getField(name)).doubleValue();
+	}
 
-    public Date getDateField(String name) throws MbException {
-        if (getField(name) instanceof MbTimestamp) {
-            return ((MbTimestamp) getField(name)).getTime();
-        } else if (getField(name) instanceof MbDate) {
-            return ((MbDate) getField(name)).getTime();
-        } else {
-            return ((MbTime) getField(name)).getTime();
-        }
-    }
+	public boolean getBooleanValue(String name) throws MbException {
+		return ((Boolean) getField(name)).booleanValue();
+	}
 
-    /**
-     * Get a date value based on a String field and a date pattern which
-     * is used for parsing. Useful for example 
-     * when handling dates with time zones, something that WMB does not currently
-     * support.
-     * 
-     * @param name The element whos value if to be retrieved
-     * @param datePattern The date pattern to use for parsning, see {@link SimpleDateFormat} 
-     * 			  for documentation of the allowed tokens
-     * @return A date based on the String field value
-     * @throws MbException
-     */
-    public Date getDateField(String name, String datePattern) throws MbException {
-    	String dateStr = getStringField(name);
-    	
-    	return parseDate(dateStr, datePattern);
-    }
+	public Date getDateField(String name) throws MbException {
+		if (getField(name) instanceof MbTimestamp) {
+			return ((MbTimestamp) getField(name)).getTime();
+		} else if (getField(name) instanceof MbDate) {
+			return ((MbDate) getField(name)).getTime();
+		} else {
+			return ((MbTime) getField(name)).getTime();
+		}
+	}
 
-    /**
-     * Get a date value based on a String field and a date pattern which
-     * is used for parsing. Useful for example 
-     * when handling dates with time zones, something that WMB does not currently
-     * support.
-     * 
-     * @param name The element whos value if to be retrieved
-     * @param datePattern The date pattern to use for parsning, see {@link SimpleDateFormat} 
-     * 			  for documentation of the allowed tokens
-     * @param fieldLength The specified length of the field, useful when input format uses 
-     * 			number fields to store dates and the leading 0 is removed in parsing
-     * @return A date based on the String field value
-     * @throws MbException
-     */
-    public Date getDateField(String name, String datePattern, int fieldLength) throws MbException {
-    	StringBuffer dateStr = new StringBuffer(getStringField(name));
-    	
-    	while(dateStr.length() < fieldLength) {
-    		dateStr.insert(0, '0');
-    	}
-    	
-    	return parseDate(dateStr.toString(), datePattern);
-    }
+	/**
+	 * Get a date value based on a String field and a date pattern which is used
+	 * for parsing. Useful for example when handling dates with time zones,
+	 * something that WMB does not currently support.
+	 * 
+	 * @param name
+	 *            The element whos value if to be retrieved
+	 * @param datePattern
+	 *            The date pattern to use for parsning, see
+	 *            {@link SimpleDateFormat} for documentation of the allowed
+	 *            tokens
+	 * @return A date based on the String field value
+	 * @throws MbException
+	 */
+	public Date getDateField(String name, String datePattern)
+			throws MbException {
+		String dateStr = getStringField(name);
 
-    private Date parseDate(String dateStr, String datePattern) throws MbException {
-    	if(dateStr != null) {
-	    	DateFormat df = new SimpleDateFormat(datePattern);
-	    	df.setLenient(false);
-	    	try {
+		return parseDate(dateStr, datePattern);
+	}
+
+	/**
+	 * Get a date value based on a String field and a date pattern which is used
+	 * for parsing. Useful for example when handling dates with time zones,
+	 * something that WMB does not currently support.
+	 * 
+	 * @param name
+	 *            The element whos value if to be retrieved
+	 * @param datePattern
+	 *            The date pattern to use for parsning, see
+	 *            {@link SimpleDateFormat} for documentation of the allowed
+	 *            tokens
+	 * @param fieldLength
+	 *            The specified length of the field, useful when input format
+	 *            uses number fields to store dates and the leading 0 is removed
+	 *            in parsing
+	 * @return A date based on the String field value
+	 * @throws MbException
+	 */
+	public Date getDateField(String name, String datePattern, int fieldLength)
+			throws MbException {
+		StringBuffer dateStr = new StringBuffer(getStringField(name));
+
+		while (dateStr.length() < fieldLength) {
+			dateStr.insert(0, '0');
+		}
+
+		return parseDate(dateStr.toString(), datePattern);
+	}
+
+	private Date parseDate(String dateStr, String datePattern)
+			throws MbException {
+		if (dateStr != null) {
+			DateFormat df = new SimpleDateFormat(datePattern);
+			df.setLenient(false);
+			try {
 				return df.parse(dateStr);
 			} catch (ParseException e) {
-				throw new NiceMbException("Failed to parse date \"" + dateStr + "\" with format \"" + datePattern + "\"");
+				throw new NiceMbException("Failed to parse date \"" + dateStr
+						+ "\" with format \"" + datePattern + "\"");
 			}
-    	} else  {
-    		return null;
-    	}
-    }
-    
-    
-    public void setStringField(String name, String value) throws MbException {
-        setField(name, value);
-    }
+		} else {
+			return null;
+		}
+	}
 
-    public void setIntField(String name, int value) throws MbException {
-        setField(name, new Integer(value));
-    }
+	public void setStringField(String name, String value) throws MbException {
+		setField(name, value);
+	}
 
-    public void setLongField(String name, long value) throws MbException {
-        setField(name, new Long(value));
-    }
+	public void setIntField(String name, int value) throws MbException {
+		setField(name, new Integer(value));
+	}
 
-    public void setFloatField(String name, float value) throws MbException {
-        setField(name, new Float(value));
-    }
+	public void setLongField(String name, long value) throws MbException {
+		setField(name, new Long(value));
+	}
 
-    public void setDoubleField(String name, double value) throws MbException {
-        setField(name, new Double(value));
-    }
+	public void setFloatField(String name, float value) throws MbException {
+		setField(name, new Float(value));
+	}
 
-    public void setBooleanField(String name, boolean value) throws MbException {
-        setField(name, new Boolean(value));
-    }
+	public void setDoubleField(String name, double value) throws MbException {
+		setField(name, new Double(value));
+	}
 
-    /**
-     * Sets the date for a known element. The date is extracted out of a Date
-     * object and an MbDate object is created.
-     * 
-     * @param name
-     *            The element whos value is to be changed.
-     * @param value
-     *            A Date object that contains a specific date.
-     * @throws MbException
-     */
-    public void setDateField(String name, Date value) throws MbException {
-        Calendar cal = new GregorianCalendar();
-        cal.setTime(value);
-        MbDate mbDate = new MbDate(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal
-                .get(Calendar.DAY_OF_MONTH));
-        setField(name, mbDate);
-    }
+	public void setBooleanField(String name, boolean value) throws MbException {
+		setField(name, new Boolean(value));
+	}
 
-    
-    /**
-     * Sets the time for a known element. The time is extracted out of a Date
-     * object and an MbTime object is created.
-     * 
-     * @param name
-     *            The element whos value is to be changed.
-     * @param value
-     *            A Date object that contains a specific time.
-     * @throws MbException
-     */
-    public void setTimeField(String name, Date value) throws MbException {
-        Calendar cal = new GregorianCalendar();
-        cal.setTime(value);
-        MbTime mbTime = new MbTime(cal.get(Calendar.HOUR), cal.get(Calendar.MINUTE), cal
-                .get(Calendar.SECOND), cal.get(Calendar.MILLISECOND));
-        setField(name, mbTime);
-    }
+	/**
+	 * Sets the date for a known element. The date is extracted out of a Date
+	 * object and an MbDate object is created.
+	 * 
+	 * @param name
+	 *            The element whos value is to be changed.
+	 * @param value
+	 *            A Date object that contains a specific date.
+	 * @throws MbException
+	 */
+	public void setDateField(String name, Date value) throws MbException {
+		Calendar cal = new GregorianCalendar();
+		cal.setTime(value);
+		MbDate mbDate = new MbDate(cal.get(Calendar.YEAR), cal
+				.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH));
+		setField(name, mbDate);
+	}
 
-    /**
-     * Sets the timestamp for a known element. The timestamp is extracted out of
-     * a Date object and an MbTimestamp object is created.
-     * 
-     * @param name
-     *            The element whos value is to be changed.
-     * @param value
-     *            A Date object that contains a specific timestamp.
-     * @throws MbException
-     */
-    public void setTimestampField(String name, Date value) throws MbException {
-        Calendar cal = new GregorianCalendar();
-        cal.setTime(value);
-        MbTimestamp mbTimestamp = new MbTimestamp(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH),
-                cal.get(Calendar.DAY_OF_MONTH), cal.get(Calendar.HOUR), cal.get(Calendar.MINUTE),
-                cal.get(Calendar.SECOND), cal.get(Calendar.MILLISECOND));
-        setField(name, mbTimestamp);
-    }
-    
-    /**
-     * Sets the timestamp for a known element. The timestamp is formated using 
-     * the provided date pattern and set as a String field. Useful for example 
-     * when handling dates with time zones, something that WMB does not currently
-     * support.
-     * 
-     * Note that this method can also be used for times and dates only.
-     * 
-     * @param name
-     *            The element whos value is to be changed.
-     * @param value
-     *            A Date object that contains a specific timestamp.
-     * @param datePattern The date pattern to use for formating, see {@link SimpleDateFormat} 
-     * 			  for documentation of the allowed tokens
-     * @throws MbException
-     */    
-    public void setTimestampField(String name, Date value, String datePattern) throws MbException {
-        DateFormat df = new SimpleDateFormat(datePattern);
-        
-        setStringField(name, df.format(value));
-    }
+	/**
+	 * Sets the time for a known element. The time is extracted out of a Date
+	 * object and an MbTime object is created.
+	 * 
+	 * @param name
+	 *            The element whos value is to be changed.
+	 * @param value
+	 *            A Date object that contains a specific time.
+	 * @throws MbException
+	 */
+	public void setTimeField(String name, Date value) throws MbException {
+		Calendar cal = new GregorianCalendar();
+		cal.setTime(value);
+		MbTime mbTime = new MbTime(cal.get(Calendar.HOUR), cal
+				.get(Calendar.MINUTE), cal.get(Calendar.SECOND), cal
+				.get(Calendar.MILLISECOND));
+		setField(name, mbTime);
+	}
+
+	/**
+	 * Sets the timestamp for a known element. The timestamp is extracted out of
+	 * a Date object and an MbTimestamp object is created.
+	 * 
+	 * @param name
+	 *            The element whos value is to be changed.
+	 * @param value
+	 *            A Date object that contains a specific timestamp.
+	 * @throws MbException
+	 */
+	public void setTimestampField(String name, Date value) throws MbException {
+		Calendar cal = new GregorianCalendar();
+		cal.setTime(value);
+		MbTimestamp mbTimestamp = new MbTimestamp(cal.get(Calendar.YEAR), cal
+				.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH), cal
+				.get(Calendar.HOUR), cal.get(Calendar.MINUTE), cal
+				.get(Calendar.SECOND), cal.get(Calendar.MILLISECOND));
+		setField(name, mbTimestamp);
+	}
+
+	/**
+	 * Sets the timestamp for a known element. The timestamp is formated using
+	 * the provided date pattern and set as a String field. Useful for example
+	 * when handling dates with time zones, something that WMB does not
+	 * currently support.
+	 * 
+	 * Note that this method can also be used for times and dates only.
+	 * 
+	 * @param name
+	 *            The element whos value is to be changed.
+	 * @param value
+	 *            A Date object that contains a specific timestamp.
+	 * @param datePattern
+	 *            The date pattern to use for formating, see
+	 *            {@link SimpleDateFormat} for documentation of the allowed
+	 *            tokens
+	 * @throws MbException
+	 */
+	public void setTimestampField(String name, Date value, String datePattern)
+			throws MbException {
+		DateFormat df = new SimpleDateFormat(datePattern);
+
+		setStringField(name, df.format(value));
+	}
 }
