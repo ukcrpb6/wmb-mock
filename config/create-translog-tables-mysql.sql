0574392c5592a0396ec0550fa4c5ad4e9577c4e1
gustav.sinder <gustav.sinder@68ca202e-fa3b-0410-97e9-59b6ee23507d>
2009-03-26 09:03:56 +0000

    git-svn-id: http://wmb-util.googlecode.com/svn/wmb-util/trunk@183 68ca202e-fa3b-0410-97e9-59b6ee23507d

--- config/create-translog-tables-mysql.sql
+++ config/create-translog-tables-mysql.sql
@@ -1,24 +1,25 @@
--- This is an example for MySQL, will require changes for DB/400
+-- This is an example for MySQL
 
 drop TABLE TRANS_LOG;
 
 CREATE TABLE TRANS_LOG (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	broker VARCHAR(100) NOT NULL,
-    message_flow VARCHAR(100) NOT NULL,
-    component VARCHAR(100),
-    level VARCHAR(10) NOT NULL,
+  message_flow VARCHAR(100) NOT NULL,
+  component VARCHAR(100),
+  level VARCHAR(10) NOT NULL,
 	message TEXT,
-    message_id VARCHAR(100),
-    exception TEXT,
-    update_time DATETIME
+  message_id VARCHAR(100),
+  exception TEXT,
+  update_time DATETIME
 );
 
 drop TABLE TRANS_LOG_BUSINESS;
+
 CREATE TABLE TRANS_LOG_BUSINESS (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    log_id INT NOT NULL,
-    business_id VARCHAR(100) NOT NULL
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  log_id INT NOT NULL,
+  business_id VARCHAR(100) NOT NULL
 );
 
 -- TODO foreign key
