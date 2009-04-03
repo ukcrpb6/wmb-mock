713909ec3ef135bf03b96a334bee1f6be07bc0ab
niklas@protocol7.com <niklas@protocol7.com@68ca202e-fa3b-0410-97e9-59b6ee23507d>
2009-04-03 11:20:25 +0000

    Scrap support for broken Oracle JDBC driver, make sure you use version 10.2.0.3
    git-svn-id: http://wmb-util.googlecode.com/svn/wmb-util/trunk@187 68ca202e-fa3b-0410-97e9-59b6ee23507d

--- src/main/java/com/googlecode/wmbutil/log/db/OracleDatabaseStrategy.java
+++ src/main/java/com/googlecode/wmbutil/log/db/OracleDatabaseStrategy.java
@@ -6,45 +6,34 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+/**
+ * Note that due to a bug in the Oracle JDBC drivers, this code only
+ * works with Oracle 10.2.0.3 drivers or newer. The Oracle 11 drivers do
+ * not currently work.
+ * So, if you get an  ArrayIndexOutOfBoundsException, make sure you use the
+ * correct Oracle JDBC driver version. See this thread for a dicussion:
+ * http://forums.oracle.com/forums/thread.jspa?messageID=2698647
+ */
 public class OracleDatabaseStrategy extends DatabaseStrategy {
 
-    protected String INSERT_SQL = "INSERT INTO TRANS_LOG (broker, message_flow, component, log_level, message, message_id, exception,  update_time, id) values(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    protected String INSERT_SQL = "INSERT INTO TRANS_LOG (id, broker, message_flow, component, log_level, message, message_id, exception,  update_time) values(TRANS_LOG_SEQ.nextval, ?, ?, ?, ?, ?, ?, ?, ?)";
     
     private String INSERT_BUSINESS_SQL = "INSERT INTO TRANS_LOG_BUSINESS (id, log_id, business_id) values(TRANS_LOG_SEQ.nextval, ?, ?)";
     
-    private int logId;
-    
-    private int getNextId(Connection conn) throws SQLException {
-        Statement idStmt = null;
-        ResultSet rs = null;
+    public PreparedStatement prepareInsertStatement(Connection conn) throws SQLException {
+        return conn.prepareStatement(INSERT_SQL, new String[]{"id"}); 
+    }
 
+    public int getGeneratedKey(Statement stmt) throws SQLException {
+        ResultSet rs = null;
         try {
-            idStmt = conn.createStatement();
-            rs = idStmt.executeQuery("SELECT TRANS_LOG_SEQ.nextval from dual");
+            rs = stmt.getGeneratedKeys();
             rs.next();
             return rs.getInt(1);
         } finally {
             DbUtil.closeQuitely(rs);
-            DbUtil.closeQuitely(idStmt);
         }
     }
-    
-    public PreparedStatement prepareInsertStatement(Connection conn) throws SQLException {
-        // this is a hack due to an ArrayIndexOutOfBoundsException in the 11g JDBC drivers:
-        // http://forums.oracle.com/forums/thread.jspa?messageID=2698647
-        // otherwise we could have embedded the nextval selection into the insert
-        // and picked up the generated value using getGeneratedKeys
-        logId = getNextId(conn);
-        
-        PreparedStatement insertStmt = conn.prepareStatement(INSERT_SQL); 
-        insertStmt.setInt(9, logId);
-        return insertStmt;
-    }
-
-    public int getGeneratedKey(Statement stmt) throws SQLException {
-        return logId;
-    }
-
     public PreparedStatement prepareInsertBusinessStatement(Connection conn) throws SQLException {
         return conn.prepareStatement(INSERT_BUSINESS_SQL); 
     }
