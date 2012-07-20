b2704b930f9b6fb9f023eead818e24705b46038d
Bob Browning <rpb6@drache.co.uk>
2012-07-20 15:55:25 +0100

    Added MQMT implementation

---
+++ src/main/java/com/googlecode/wmbutil/messages/MqMessageType.java
@@ -0,0 +1,20 @@
+package com.googlecode.wmbutil.messages;
+
+/**
+ * @author Bob Browning <bob.browning@pressassociation.com>
+ */
+@SuppressWarnings("UnusedDeclaration")
+public class MqMessageType {
+
+    public static final int MQMT_APPL_FIRST = 65536;
+    public static final int MQMT_APPL_LAST = 999999999;
+    public static final int MQMT_DATAGRAM = 8;
+    public static final int MQMT_MQE_FIELDS = 113;
+    public static final int MQMT_MQE_FIELDS_FROM_MQE = 112;
+    public static final int MQMT_REPLY = 2;
+    public static final int MQMT_REPORT = 4;
+    public static final int MQMT_REQUEST = 1;
+    public static final int MQMT_SYSTEM_FIRST = 1;
+    public static final int MQMT_SYSTEM_LAST = 65535;
+
+}
