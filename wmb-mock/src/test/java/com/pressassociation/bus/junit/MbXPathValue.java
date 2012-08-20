7857b1057c438c13e3c17840040b4477264e9527
Bob Browning <rpb6@drache.co.uk>
2012-08-20 10:28:49 +0100

    Removed old mock components

--- wmb-mock/src/test/java/com/pressassociation/bus/junit/MbXPathValue.java
+++
@@ -1,8 +0,0 @@
-package com.pressassociation.bus.junit;
-
-/**
- * @author Bob Browning <bob.browning@pressassociation.com>
- */
-public interface MbXPathValue {
-    String getValue();
-}
--- wmb-mock/src/test/java/com/pressassociation/bus/junit/PreMock.java
+++
@@ -1,9 +0,0 @@
-package com.pressassociation.bus.junit;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface PreMock {
-    Class<?>[] value();
-}
