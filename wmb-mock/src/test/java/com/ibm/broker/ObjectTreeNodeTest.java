8b5d45c6c2f6c6bea3c93755fa03afe8234efb95
Bob Browning <rpb6@drache.co.uk>
2012-08-21 21:28:11 +0100

    Updated ObjectTreeNode implementation

--- wmb-mock/src/test/java/com/ibm/broker/ObjectTreeNodeTest.java
+++ wmb-mock/src/test/java/com/ibm/broker/ObjectTreeNodeTest.java
@@ -4,6 +4,8 @@ import com.ibm.broker.plugin.ObjectTreeNode;
 import junit.framework.Assert;
 import org.junit.Test;
 
+import java.util.ListIterator;
+
 /**
  * @author Bob Browning <bob.browning@pressassociation.com>
  */
@@ -27,6 +29,57 @@ public class ObjectTreeNodeTest {
 
         Assert.assertEquals(6, tree.size());
         Assert.assertEquals(5, newNode.size());
+
+        Assert.assertNull(tree.getParent());
+
+        for (ObjectTreeNode<Integer> node : tree) {
+            Assert.assertEquals(tree, node.getParent());
+        }
+
+        for (ObjectTreeNode<Integer> node : newNode) {
+            Assert.assertEquals(newNode, node.getParent());
+        }
+
+        Assert.assertNull(tree.getPreviousSibling());
+        Assert.assertNull(tree.getNextSibling());
+
+        Assert.assertNotNull(newNode.getPreviousSibling());
+        Assert.assertNull(newNode.getNextSibling());
+
+        Assert.assertNull(newNode.get(0).getPreviousSibling());
+        Assert.assertNotNull(newNode.get(0).getNextSibling());
+
+        int i = 0;
+        ObjectTreeNode<Integer> testNode = newNode.get(i);
+        while(testNode != null && ++i < 6) {
+            testNode = testNode.getNextSibling();
+        }
+        Assert.assertEquals(5, i);
+
+        testNode = newNode.get(4);
+        while(testNode != null && i-- > 0) {
+            testNode = testNode.getPreviousSibling();
+        }
+        Assert.assertEquals(0, i);
+
+        ListIterator<ObjectTreeNode<Integer>> iter = newNode.listIterator();
+        while(iter.hasNext()) {
+            ObjectTreeNode<Integer> o = iter.next();
+            int index = newNode.indexOf(o);
+            System.out.println(index + " -> " + o.getValue());
+            if(index == 2 && o.getValue() == 3) {
+                System.out.println("Adding after value " + o.getValue());
+                ObjectTreeNode<Integer> newNode99 = new ObjectTreeNode<Integer>();
+                newNode99.setValue(99);
+                iter.add(newNode99);
+                iter.previous();
+            }
+        }
+        ObjectTreeNode<Integer> newNode99 = new ObjectTreeNode<Integer>();
+        newNode99.setValue(99);
+        iter.add(newNode99);
+        Assert.assertEquals(99, (int) newNode.get(3).getValue());
+        Assert.assertEquals(99, (int) newNode.getLast().getValue());
     }
 
     public static <E> ObjectTreeNode<E> objectTreeNode(E value) {
